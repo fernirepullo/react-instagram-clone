f5d3ecf2ff2ece9399f02a3919b8574e16e93dff
Fernando Repullo <frepvil862@g.educaand.es>
2022-01-13 13:37:34 +0100

    13/01 - Commit 1 - Configuración inicial

---
+++ src/Configuration/Firebase_Configuration.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlIUbTHMDyIeiFkfF5TwU38tl1Xz57RE4",
+  authDomain: "fir-test-644c3.firebaseapp.com",
+  projectId: "fir-test-644c3",
+  storageBucket: "fir-test-644c3.appspot.com",
+  messagingSenderId: "747871511572",
+  appId: "1:747871511572:web:f85f988b9b4e44bdfcb301"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
